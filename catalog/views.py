57a1f97dafe3f8471abf1259e2add8e39d8961b0
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-01-15 20:42:27 +0500

    delete contacts

--- catalog/views.py
+++ catalog/views.py
@@ -35,18 +35,3 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_list')
-
-
-
-
-def home(request):
-    return render(request, 'home.html')
-
-def contacts(request):
-    return render(request, 'contacts.html')
-
-def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'home.html')
